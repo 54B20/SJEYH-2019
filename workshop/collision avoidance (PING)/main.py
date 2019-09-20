6710a1533c4cc5efa97371c42098eb8bbb7fb165
Brian Geiger <b_geiger@u.pacific.edu>
2019-09-20 15:00:01 -0700

    Functioning PING sensor solution

--- workshop/collision avoidance (PING)/main.py
+++ workshop/collision avoidance (PING)/main.py
@@ -1,12 +1,25 @@
+"""
+FILE: main.py
+DESC: Working example of Trinket M0 PING sensor
+"""
+
 import board
+import busio
 from time import sleep
 from adafruit_hcsr04 import HCSR04
+
+threshold = 10.0
+delay = 0.59    # 59ms (a prime number to discurage race conditions)
+uart = busio.UART(board.TX, board.RX, baudrate=115200)
 
 with HCSR04(board.D2, board.D1) as sonar:
     while True:
         try:
             print(sonar.distance)
-            sleep(1)
+            if(sonar.distance < threshold):
+                uart.write("S")
+            # what's my reset/all-clear condition????
+            sleep(delay)
         except Exception:
             pass
 
